4bccd21bbc781976805d7f28e4b93e404aedc703
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-06-11 17:26:10 -0400

    dataloader preprocessing gpu

--- KWS_LSTM.py
+++ KWS_LSTM.py
@@ -24,7 +24,7 @@ parser.add_argument("--dataset-path-train", type=str, default='data.nosync/speec
 parser.add_argument("--dataset-path-test", type=str, default='data.nosync/speech_commands_test_set_v0.02', help='Path to Dataset')
 parser.add_argument("--batch-size", type=int, default=512, help='Batch Size')
 parser.add_argument("--validation-size", type=int, default=1000, help='Number of batches used for validation')
-parser.add_argument("--epochs", type=int, default=2, help='Epochs')
+parser.add_argument("--epochs", type=int, default=30, help='Epochs')
 parser.add_argument("--lr-divide", type=int, default=10000, help='Learning Rate divide')
 parser.add_argument("--hidden", type=int, default=200, help='Number of hidden LSTM units') 
 parser.add_argument("--learning-rate", type=float, default=0.0005, help='Dropout Percentage')
