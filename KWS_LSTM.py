0356f0db846ae63a00bd3558b8bb1f32fb398d48
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-08-11 22:55:23 -0400

    half qsig exp

--- KWS_LSTM.py
+++ KWS_LSTM.py
@@ -125,10 +125,10 @@ class QuantFunc(torch.autograd.Function):
         # quantization
 
         # new experimental approach 
-        # if train:
-        #     return .5 * quant(x, wb, sign) + .5 * x
-        # else:
-        return quant(x, wb, sign)
+        if train:
+            return .5 * quant(x, wb, sign) + .5 * x
+        else:
+            return quant(x, wb, sign)
 
     @staticmethod
     def backward(ctx, grad_output):
@@ -382,7 +382,7 @@ for e, ((x_data, y_label),(x_vali, y_vali)) in enumerate(zip(islice(train_datalo
     start_time = time.time()
     x_data, y_label = pre_processing(x_data, y_label, device, mfcc_cuda, args.std_scale)
 
-    output = model(x_data, train = False)
+    output = model(x_data, train = True)
     # cross entropy loss
     # if e < args.CE_train:
     #     loss_val = loss_fn(output.view(-1, output.shape[-1]), torch.tensor(y_label.tolist()*output.shape[0]).to(device))
--- sweeps.py
+++ sweeps.py
@@ -1,6 +1,6 @@
 import os
 
-ident_word = "QSigmoid"
+ident_word = "HalfQSigmoid"
 
 part1 = "#!/bin/csh \n#$ -M cschaef6@nd.edu \n#$ -m abe\n#$ -q gpu@@joshi\n#$ -l gpu_card=1\n#$ -N "
 
@@ -19,9 +19,11 @@ part4 = ".txt\nmodule load python\nsetenv OMP_NUM_THREADS $NSLOTS\npython KWS_LS
 
 #sweep_parameters = {'n-mfcc':[40, 50, 60, 80, 100]}
 
-# sweep_parameters = {'hop-length':[150, 200, 250, 300, 350]}
+#sweep_parameters = {'hop-length':[150, 200, 250, 300, 350]}
 
-sweep_parameters = {'n-mfcc':[40, 70, 100], 'hop-length':[200, 270], 'std-scale':[1,3,4]}
+#sweep_parameters = {'n-mfcc':[40, 70, 100], 'hop-length':[200, 270], 'std-scale':[1,3,4]}
+
+sweep_parameters = {'n-mfcc':[40]}
 
 trials = 3
 
