01c120e2d5744ea2691bb307d1cffff3dc93beba
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-08-14 00:00:10 -0400

    gpu specify

--- sweeps.py
+++ sweeps.py
@@ -2,7 +2,8 @@ import os
 
 ident_word = "HH"
 
-part1 = "#!/bin/csh \n#$ -M cschaef6@nd.edu \n#$ -m abe\n#$ -q gpu@@joshi\n#$ -l gpu_card=1\n#$ -N "
+part1 = "#!/bin/csh \n#$ -M cschaef6@nd.edu \n#$ -m abe\n#$ -q " 
+part11 = "\n#$ -l gpu_card=1\n#$ -N "
 
 part2 = "\n#$ -o ./logs/output_"+ident_word+"_"
 
@@ -29,10 +30,16 @@ sweep_parameters = {'n-mfcc':[40]}
 
 trials = 3
 
+avail_q = ['gpu@qa-rtx6k-040.crc.nd.edu', 'gpu@qa-rtx6k-041.crc.nd.edu']
+q_counter = 0
+
 for i in range(trials):
     for variable in sweep_parameters:
         for value in sweep_parameters[variable]:
             name = ident_word + "_" +variable + "_" + str(value) + "_" + str(i)
             with open('jobscripts/'+name+'.script', 'w') as f:
-                f.write(part1 + name + part2 + name + part3 + name + part4 + " --" + variable + " " + str(value))
+                f.write(part1 + avail_q[q_counter] + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " " + str(value))
             os.system("qsub "+ 'jobscripts/'+name+'.script')
+            q_counter += 1
+            if q_counter > len(avail_q):
+                q_counter = 0
