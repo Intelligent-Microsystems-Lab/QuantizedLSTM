95464e44ad4964c2bb5843833a7cc1dc2d5fbc8f
Clemens JS Schafer <clemens@clemenss-mbp.dhcp.nd.edu>
2020-09-26 17:50:17 -0400

    log_mel True

--- dataloader.py
+++ dataloader.py
@@ -194,6 +194,9 @@ class SpeechCommandsGoogle(Dataset):
         else:
             uniform_waveform = waveform
 
-        import pdb; pdb.set_trace()
+        
+
+        if len(self.list_of_y[idx]) != 1:
+            import pdb; pdb.set_trace()
         return uniform_waveform[0].t(), self.list_of_y[idx]
 
