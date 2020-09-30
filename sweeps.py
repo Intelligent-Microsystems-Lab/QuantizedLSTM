98262307b3fcf0926fb093a13acfd20d68fbba11
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-09-30 00:00:21 -0500

    std and mean

--- sweeps.py
+++ sweeps.py
@@ -1,6 +1,6 @@
 import os
 
-ident_word = "ARM_Replica"
+ident_word = "ARM_Replica_um"
 
 part1 = "#!/bin/csh \n#$ -M cschaef6@nd.edu \n#$ -m abe\n#$ -q " 
 part11 = "\n#$ -l gpu_card=1\n#$ -N "
@@ -24,8 +24,9 @@ part4 = ".txt\nmodule load python\nsetenv OMP_NUM_THREADS $NSLOTS\npython KWS_LS
 
 #sweep_parameters = {'n-mfcc':[40, 70, 100], 'hop-length':[200, 270], 'std-scale':[1,3,4]}
 
-sweep_parameters = {'quant-inp':[2,3,4,5,6,7,8,12,16]}
+#sweep_parameters = {'quant-inp':[2,3,4,5,6,7,8,12,16]}
 
+sweep_parameters = {'quant-actMVM': [0]}
 
 trials = 3
 
