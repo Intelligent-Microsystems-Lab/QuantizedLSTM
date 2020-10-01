11f0fd547d347e88e47c5d6e3367a949556552e4
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-10-01 18:29:06 -0400

    128

--- KWS_LSTM.py
+++ KWS_LSTM.py
@@ -123,7 +123,7 @@ for e, (x_data, y_label) in enumerate(islice(train_dataloader, epoch_list[-1])):
     # train
     x_data, y_label = pre_processing(x_data, y_label, device, mfcc_cuda, args.std_scale, args.inp_mean, args.inp_std)
 
-    x_data = quant_pass(x_data, args.quant_inp, True, False)
+    #x_data = quant_pass(x_data, args.quant_inp, True, False)
 
     output = model(x_data, train = True)
     
@@ -141,7 +141,7 @@ for e, (x_data, y_label) in enumerate(islice(train_dataloader, epoch_list[-1])):
         for val_e, (x_vali, y_vali) in enumerate(validation_dataloader):
             x_data, y_label = pre_processing(x_vali, y_vali, device, mfcc_cuda, args.std_scale, args.inp_mean, args.inp_std)
 
-            x_data = quant_pass(x_data, args.quant_inp, True, False)
+            #x_data = quant_pass(x_data, args.quant_inp, True, False)
 
             output = model(x_data, train = False)
             temp_list.append((output.argmax(dim=1) == y_label).float().mean().item())
@@ -179,7 +179,7 @@ model.noise_level = args.noise_injectionI
 for i_batch, sample_batch in enumerate(test_dataloader):
     x_data, y_label = sample_batch
     x_data, y_label = pre_processing(x_data, y_label, device, mfcc_cuda, args.std_scale, args.inp_mean, args.inp_std)
-    x_data = quant_pass(x_data, args.quant_inp, True, False)
+    #x_data = quant_pass(x_data, args.quant_inp, True, False)
 
     output = model(x_data, train = False)
     acc_aux.append((output.argmax(dim=1) == y_label))
