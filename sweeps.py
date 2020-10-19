dad83ef4ceca38deacd29ee425d35f86f004db7e
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-10-18 23:23:45 -0400

    lr

--- sweeps.py
+++ sweeps.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-ident_word = "lr_sweep"
+ident_word = "lr_bmm"
 
 part1 = "#!/bin/csh \n#$ -M cschaef6@nd.edu \n#$ -m abe\n#$ -q " 
 part11 = "\n#$ -l gpu_card=1\n#$ -N "
