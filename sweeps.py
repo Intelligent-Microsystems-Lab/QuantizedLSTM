0b89c8315557a0d83f84bcaa42725f9c027476c9
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-10-18 23:27:15 -0400

    lr

--- sweeps.py
+++ sweeps.py
@@ -36,7 +36,7 @@ part4 = ".txt\nmodule load python\nsetenv OMP_NUM_THREADS $NSLOTS\npython KWS_LS
 
 #sweep_parameters = {'noise-injectionT': list(np.round(np.arange(0.10, .20, .01), 2 ))   }
 
-sweep_parameters = {'learning-rate': ['0.001,0.0002,0.00004', '0.002,0.0004,0.00008', '0.01,0.005,0.001', '0.0001,0.00005,0.00001', '0.0005,0.0001,0.00002']   }
+sweep_parameters = {'learning-rate': ['\"0.001,0.0002,0.00004\"', '\"0.002,0.0004,0.00008\"', '\"0.01,0.005,0.001\"', '\"0.0001,0.00005,0.00001\"', '\"0.0005,0.0001,0.00002\"']   }
 
 trials = 3
 
