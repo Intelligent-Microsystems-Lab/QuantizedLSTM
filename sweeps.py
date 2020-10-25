31c72ee55c652b9fca15619f3f77ee942f08bcb1
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-10-24 23:49:20 -0500

    bias rows

--- sweeps.py
+++ sweeps.py
@@ -88,8 +88,8 @@ trials = 3
 
 random_seeds = [193012823 ,235899598, 8627169, 103372330, 14339038, 221706254, 46192121, 188833202, 37306063, 171928928]
 
-#avail_q = ['gpu@qa-rtx6k-040.crc.nd.edu', 'gpu@qa-rtx6k-041.crc.nd.edu']
-avail_q = ['gpu@@joshi']
+avail_q = ['gpu@qa-rtx6k-040.crc.nd.edu', 'gpu@qa-rtx6k-041.crc.nd.edu', 'gpu@ta-titanv-001.crc.nd.edu']
+#avail_q = ['gpu@@joshi']
 q_counter = 0
 
 for i in range(trials):
