e5d3c835717d128d611848d7a46f1128a8c17392
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-11-01 12:05:39 -0600

    hidden

--- plots/paper_figs.py
+++ plots/paper_figs.py
@@ -68,17 +68,15 @@ plt.close()
 # Ablation Study Energy + Accuracy
 ############
 
-inp_bits = pd.read_csv("QuantizedLSTMs - InpBits.csv")
-out_bits = pd.read_csv("QuantizedLSTMs - OutBits.csv")
-nm_bits = pd.read_csv("QuantizedLSTMs - NMbits.csv")
-nmsb = pd.read_csv("QuantizedLSTMs - NMSB.csv")
-
+inp_bits = (pd.read_csv("QuantizedLSTMs - M0_InpBits.csv"), pd.read_csv("QuantizedLSTMs - M1_InpBits.csv"))
+out_bits = (pd.read_csv("QuantizedLSTMs - M0_OutBits.csv"), pd.read_csv("QuantizedLSTMs - M1_OutBits.csv"))
+nm_bits  = (pd.read_csv("QuantizedLSTMs - M0_NMbits.csv"), pd.read_csv("QuantizedLSTMs - M1_NMbits.csv"))
+nmsb     = (pd.read_csv("QuantizedLSTMs - M0_NMSB.csv"), pd.read_csv("QuantizedLSTMs - M1_NMSB.csv"))
 
 
 plt.rcParams["font.weight"] = "bold"
 plt.rcParams["axes.labelweight"] = "bold"
 plt.rc('font', size='14')
-
 
 plt.clf()
 plt.rc('font', family='sans-serif')
