77ffb1a49076573590e1ca998e9a70dd2ccad64d
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-11-01 23:49:58 -0500

    sweep

--- sweeps.py
+++ sweeps.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-ident_word = "hidden"
+ident_word = "digital"
 
 part1 = "#!/bin/csh \n#$ -M cschaef6@nd.edu \n#$ -m abe\n#$ -q " 
 part11 = "\n#$ -l gpu_card=1\n#$ -N "
