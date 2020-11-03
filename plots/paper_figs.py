a27732461158ad252adb210936664d7819ab30ab
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-11-03 11:11:53 -0500

    sweep

--- plots/paper_figs.py
+++ plots/paper_figs.py
@@ -3,30 +3,103 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+sym_marker_size = 128
+
+# https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain
+def convex_hull(points):
+    """Computes the convex hull of a set of 2D points.
+
+    Input: an iterable sequence of (x, y) pairs representing the points.
+    Output: a list of vertices of the convex hull in counter-clockwise order,
+      starting from the vertex with the lexicographically smallest coordinates.
+    Implements Andrew's monotone chain algorithm. O(n log n) complexity.
+    """
+
+    # Sort the points lexicographically (tuples are compared lexicographically).
+    # Remove duplicates to detect the case we have just one unique point.
+    points = sorted(set(points))
+
+    # Boring case: no points or a single point, possibly repeated multiple times.
+    if len(points) <= 1:
+        return points
+
+    # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
+    # Returns a positive value, if OAB makes a counter-clockwise turn,
+    # negative for clockwise turn, and zero if the points are collinear.
+    def cross(o, a, b):
+        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+    # Build upper hull
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+
+    # Concatenation of the lower and upper hulls gives the convex hull.
+    # Last point of each list is omitted because it is repeated at the beginning of the other list. 
+    return upper
+
+
+
+# CIM results
+inp_bits = (pd.read_csv("QuantizedLSTMs - M0_InpBits.csv").dropna(), pd.read_csv("QuantizedLSTMs - M1_InpBits.csv").dropna())
+out_bits = (pd.read_csv("QuantizedLSTMs - M0_OutBits.csv").dropna(), pd.read_csv("QuantizedLSTMs - M1_OutBits.csv").dropna())
+nm_bits  = (pd.read_csv("QuantizedLSTMs - M0_NMbits.csv").dropna(), pd.read_csv("QuantizedLSTMs - M1_NMbits.csv").dropna())
+nmsb     = (pd.read_csv("QuantizedLSTMs - M0_NMSB.csv").dropna(), pd.read_csv("QuantizedLSTMs - M1_NMSB.csv").dropna())
+hidden   = (pd.read_csv("QuantizedLSTMs - M0_hidden.csv").dropna(), pd.read_csv("QuantizedLSTMs - M1_hidden.csv").dropna())
+
+hidden = nm_bits
+nmsb = nm_bits
+
+max_acc = max(inp_bits[0][['#1','#2','#3']].max().max(), inp_bits[1][['#1','#2','#3']].max().max(), out_bits[0][['#1','#2','#3']].max().max(), out_bits[1][['#1','#2','#3']].max().max(), nm_bits[0][['#1','#2','#3']].max().max(), nm_bits[1][['#1','#2','#3']].max().max(), nmsb[0][['#1','#2','#3']].max().max(), nmsb[1][['#1','#2','#3']].max().max(), hidden[0][['#1','#2','#3']].max().max(), hidden[1][['#1','#2','#3']].max().max() )
+min_acc = min(inp_bits[0][['#1','#2','#3']].min().min(), inp_bits[1][['#1','#2','#3']].min().min(), out_bits[0][['#1','#2','#3']].min().min(), out_bits[1][['#1','#2','#3']].min().min(), nm_bits[0][['#1','#2','#3']].min().min(), nm_bits[1][['#1','#2','#3']].min().min(), nmsb[0][['#1','#2','#3']].min().min(), nmsb[1][['#1','#2','#3']].min().min(), hidden[0][['#1','#2','#3']].min().min(), hidden[1][['#1','#2','#3']].min().min() )
+max_j = max(inp_bits[0][['uJ']].max().max(), inp_bits[1][['uJ']].max().max(), out_bits[0][['uJ']].max().max(), out_bits[1][['uJ']].max().max(), nm_bits[0][['uJ']].max().max(), nm_bits[1][['uJ']].max().max(), nmsb[0][['uJ']].max().max(), nmsb[1][['uJ']].max().max(), hidden[0][['uJ']].max().max(), hidden[1][['uJ']].max().max() )
+min_j = min(inp_bits[0][['uJ']].min().min(), inp_bits[1][['uJ']].min().min(), out_bits[0][['uJ']].min().min(), out_bits[1][['uJ']].min().min(), nm_bits[0][['uJ']].min().min(), nm_bits[1][['uJ']].min().min(), nmsb[0][['uJ']].min().min(), nmsb[1][['uJ']].min().min(), hidden[0][['uJ']].min().min(), hidden[1][['uJ']].min().min() )
+
+# digital results
+d_bits = (pd.read_csv("QuantizedLSTMs - M0_dbits.csv").dropna(), pd.read_csv("QuantizedLSTMs - M1_dbits.csv").dropna())
+d_nmsb = (pd.read_csv("QuantizedLSTMs - M0_dmsb.csv").dropna(), pd.read_csv("QuantizedLSTMs - M1_dmsb.csv").dropna())
+d_hidden  = (pd.read_csv("QuantizedLSTMs - M0_dhidden.csv").dropna(), pd.read_csv("QuantizedLSTMs - M1_dhidden.csv").dropna())
+
 
 ############
-# Effiient Frontier
+# Efficient Frontier
 ############
 
-inp_bits = pd.read_csv("QuantizedLSTMs - InpBits.csv")
-out_bits = pd.read_csv("QuantizedLSTMs - OutBits.csv")
-nm_bits = pd.read_csv("QuantizedLSTMs - NMbits.csv")
-nmsb = pd.read_csv("QuantizedLSTMs - NMSB.csv")
+#M0
+x = np.concatenate([inp_bits[0]["uJ"], out_bits[0]["uJ"], nm_bits[0]["uJ"], nmsb[0]["uJ"], hidden[0]["uJ"]])
+y = np.concatenate([inp_bits[0][['#1', '#2', '#3']].mean(1), out_bits[0][['#1', '#2', '#3']].mean(1), nm_bits[0][['#1', '#2', '#3']].mean(1), nmsb[0][['#1', '#2', '#3']].mean(1), hidden[0][['#1', '#2', '#3']].mean(1)])
+n = np.concatenate([[str(x)+'ib' for x in inp_bits[0]["Input Bits"]], [str(x)+'ob' for x in out_bits[0]["Output Bits"]], [str(x)+'nc' for x in nm_bits[0]["Non CIM bits"]], [str(x)+'msb' for x in nmsb[0]["Non CIM bits"]], [str(x)+'hi' for x in nmsb[0]["Non CIM bits"]]])
+
+points_m0 = [(x[i], y[i]) for i in range(len(x))] 
+hull_m0 = reversed(convex_hull(points_m0))
+xcim0, ycim0 =zip(*hull_m0)
 
 
-x = np.concatenate([inp_bits["uJ"], out_bits["uJ"], nm_bits["uJ"], nmsb["uJ"]])
+#M1
+x = np.concatenate([inp_bits[1]["uJ"], out_bits[1]["uJ"], nm_bits[1]["uJ"], nmsb[1]["uJ"], hidden[1]["uJ"]])
+y = np.concatenate([inp_bits[1][['#1', '#2', '#3']].mean(1), out_bits[1][['#1', '#2', '#3']].mean(1), nm_bits[1][['#1', '#2', '#3']].mean(1), nmsb[1][['#1', '#2', '#3']].mean(1), hidden[1][['#1', '#2', '#3']].mean(1)])
+n = np.concatenate([[str(x)+'ib' for x in inp_bits[1]["Input Bits"]], [str(x)+'ob' for x in out_bits[1]["Output Bits"]], [str(x)+'nc' for x in nm_bits[1]["Non CIM bits"]], [str(x)+'msb' for x in nmsb[1]["Non CIM bits"]], [str(x)+'hi' for x in nmsb[1]["Non CIM bits"]]])
 
-y = np.concatenate([inp_bits[['#1', '#2', '#3']].mean(1), out_bits[['#1', '#2', '#3']].mean(1), nm_bits[['#1', '#2', '#3']].mean(1), nmsb[['#1', '#2', '#3']].mean(1)])
+points_m1 = [(x[i], y[i]) for i in range(len(x))] 
+hull_m1 = reversed(convex_hull(points_m1))
+xcim1, ycim1 =zip(*hull_m1)
 
-n = np.concatenate([[x+'ib' for x in inp_bits["Input Bits"]], [x+'ob' for x in out_bits["Output Bits"]], [x+'nc' for x in nm_bits["Non CIM bits"]], [str(x)+'msb' for x in nmsb["N MSB"]]])
+# #digital M0
+# x = np.concatenate([inp_bits["uJ"], out_bits["uJ"], nm_bits["uJ"], nmsb["uJ"]])
+# y = np.concatenate([inp_bits[['#1', '#2', '#3']].mean(1), out_bits[['#1', '#2', '#3']].mean(1), nm_bits[['#1', '#2', '#3']].mean(1), nmsb[['#1', '#2', '#3']].mean(1)])
+# n = np.concatenate([[x+'ib' for x in inp_bits["Input Bits"]], [x+'ob' for x in out_bits["Output Bits"]], [x+'nc' for x in nm_bits["Non CIM bits"]], [str(x)+'msb' for x in nmsb["N MSB"]]])
 
-# big scatter (?)
+# #digital M1
+# x = np.concatenate([inp_bits["uJ"], out_bits["uJ"], nm_bits["uJ"], nmsb["uJ"]])
+# y = np.concatenate([inp_bits[['#1', '#2', '#3']].mean(1), out_bits[['#1', '#2', '#3']].mean(1), nm_bits[['#1', '#2', '#3']].mean(1), nmsb[['#1', '#2', '#3']].mean(1)])
+# n = np.concatenate([[x+'ib' for x in inp_bits["Input Bits"]], [x+'ob' for x in out_bits["Output Bits"]], [x+'nc' for x in nm_bits["Non CIM bits"]], [str(x)+'msb' for x in nmsb["N MSB"]]])
 
 
 plt.rcParams["font.weight"] = "bold"
 plt.rcParams["axes.labelweight"] = "bold"
 plt.rc('font', size='14')
-
 
 plt.clf()
 plt.rc('font', family='sans-serif')
@@ -35,8 +108,6 @@ plt.rc('font', size='12')
 fig, axes = plt.subplots(nrows=1, ncols=1) #
 
 
-#plt.rcParams['axes.labelsize'] = 16
-#plt.rcParams['axes.labelweight'] = 'bold'
 
 for axis in ['bottom','left']:
   axes.spines[axis].set_linewidth(2)
@@ -46,32 +117,28 @@ axes.xaxis.set_tick_params(width=2)
 axes.yaxis.set_tick_params(width=2)
 
 
+axes.plot(xcim0, ycim0,'x-',color= 'red', label="CIM (M0)")
+axes.plot(xcim1, ycim1,'x-',color= 'blue', label="CIM (M1)")
+# axes.plot(,'x-',color=, label="Digital (M0)")
+# axes.plot(,'x-',color=, label="Digital (M1)")
 
-axes.scatter(x, y)
 
-for i, txt in enumerate(n):
-    axes.annotate(txt, (x[i], y[i]))
 
-#axes.plot(dataN['acc']['test3'], label = 'FP (94.10%)',linewidth=2)
-#axes.plot(dataB['acc']['test3'], label = 'Quantized (91.67%)',linewidth=2)
 axes.set_xlabel('uJ')
 axes.set_ylabel('Accuracy')
-axes.legend()
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.17), ncol=4, frameon=False)
 
-plt.legend(frameon=False)
 plt.tight_layout()
-plt.savefig('big_scatter.png')
+plt.savefig('frontiers.png')
 plt.close()
+
+
 
 
 ############
 # Ablation Study Energy + Accuracy
 ############
 
-inp_bits = (pd.read_csv("QuantizedLSTMs - M0_InpBits.csv"), pd.read_csv("QuantizedLSTMs - M1_InpBits.csv"))
-out_bits = (pd.read_csv("QuantizedLSTMs - M0_OutBits.csv"), pd.read_csv("QuantizedLSTMs - M1_OutBits.csv"))
-nm_bits  = (pd.read_csv("QuantizedLSTMs - M0_NMbits.csv"), pd.read_csv("QuantizedLSTMs - M1_NMbits.csv"))
-nmsb     = (pd.read_csv("QuantizedLSTMs - M0_NMSB.csv"), pd.read_csv("QuantizedLSTMs - M1_NMSB.csv"))
 
 
 plt.rcParams["font.weight"] = "bold"
@@ -85,8 +152,71 @@ plt.rc('font', size='12')
 fig, axes = plt.subplots(nrows=1, ncols=1) #
 
 
-#plt.rcParams['axes.labelsize'] = 16
-#plt.rcParams['axes.labelweight'] = 'bold'
+for axis in ['bottom','left']:
+  axes.spines[axis].set_linewidth(2)
+for axis in ['top','right']:
+  axes.spines[axis].set_linewidth(0)
+axes.xaxis.set_tick_params(width=2)
+axes.yaxis.set_tick_params(width=2)
+
+
+sc1 = axes.scatter([.5]*7, list(inp_bits[0]['Input Bits']), c = list(inp_bits[0][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+sc2 = axes.scatter([1.5]*7, list(out_bits[0]['Output Bits']), c = list(out_bits[0][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+sc3 = axes.scatter([2.5]*11, list(nm_bits[0]['Non CIM bits']), c = list(nm_bits[0][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+sc4 = axes.scatter([3.5]*11, list(nmsb[0]['Non CIM bits']), c = list(nmsb[0][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+sc5 = axes.scatter([4.5]*11, list(hidden[0]['Non CIM bits']), c = list(nmsb[0][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+
+
+sc6 = axes.scatter([1]*7, list(inp_bits[1]['Input Bits']), c = list(inp_bits[1][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+sc7 = axes.scatter([2]*7, list(out_bits[1]['Output Bits']), c = list(out_bits[1][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+sc8 = axes.scatter([3]*11, list(nm_bits[1]['Non CIM bits']), c = list(nm_bits[1][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+sc9 = axes.scatter([4]*11, list(nmsb[1]['Non CIM bits']), c = list(nmsb[1][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+sc10 = axes.scatter([5]*11, list(hidden[1]['Non CIM bits']), c = list(nmsb[1][['#1','#2','#3']].mean(1)) , marker = "s",  vmin=min_acc, vmax=max_acc, s=sym_marker_size)
+
+axes.set_xlabel('')
+axes.set_ylabel('# Bits/Blocks/Hidden')
+
+plt.xticks([.5, 1., 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
+
+
+labels = [item.get_text() for item in axes.get_xticklabels()]
+labels[0] = 'Input Bits (M0)'
+labels[1] = 'Input Bits (M1)'
+labels[2] = 'Output Bits (M0)'
+labels[3] = 'Output Bits (M1)'
+labels[4] = 'Non CIM Bits (M0)'
+labels[5] = 'Non CIM Bits (M1)'
+labels[6] = 'Blocks (M0)'
+labels[7] = 'Blocks (M1)'
+labels[8] = 'Hidden (M0)'
+labels[9] = 'Hidden (M1)'
+
+axes.set_xticklabels(labels)
+
+plt.xticks(rotation=55,ha='right')
+cbar = plt.colorbar(sc1)
+cbar.ax.set_xlabel('Test \nAccuracy')
+
+plt.legend(frameon=False)
+plt.tight_layout()
+plt.savefig('acc_abl.png')
+plt.close()
+
+
+
+
+
+
+plt.rcParams["font.weight"] = "bold"
+plt.rcParams["axes.labelweight"] = "bold"
+plt.rc('font', size='14')
+
+plt.clf()
+plt.rc('font', family='sans-serif')
+plt.rc('font', weight='bold')
+plt.rc('font', size='12')
+fig, axes = plt.subplots(nrows=1, ncols=1) #
+
 
 for axis in ['bottom','left']:
   axes.spines[axis].set_linewidth(2)
@@ -96,20 +226,44 @@ axes.xaxis.set_tick_params(width=2)
 axes.yaxis.set_tick_params(width=2)
 
 
+sc1 = axes.scatter([.5]*7, list(inp_bits[0]['Input Bits']), c = list(inp_bits[0][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
+sc2 = axes.scatter([1.5]*7, list(out_bits[0]['Output Bits']), c = list(out_bits[0][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
+sc3 = axes.scatter([2.5]*11, list(nm_bits[0]['Non CIM bits']), c = list(nm_bits[0][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
+sc4 = axes.scatter([3.5]*11, list(nmsb[0]['Non CIM bits']), c = list(nmsb[0][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
+sc5 = axes.scatter([4.5]*11, list(hidden[0]['Non CIM bits']), c = list(nmsb[0][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
 
-axes.scatter(x, y)
 
-for i, txt in enumerate(n):
-    axes.annotate(txt, (x[i], y[i]))
+sc6 = axes.scatter([1]*7, list(inp_bits[1]['Input Bits']), c = list(inp_bits[1][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
+sc7 = axes.scatter([2]*7, list(out_bits[1]['Output Bits']), c = list(out_bits[1][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
+sc8 = axes.scatter([3]*11, list(nm_bits[1]['Non CIM bits']), c = list(nm_bits[1][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
+sc9 = axes.scatter([4]*11, list(nmsb[1]['Non CIM bits']), c = list(nmsb[1][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
+sc10 = axes.scatter([5]*11, list(hidden[1]['Non CIM bits']), c = list(nmsb[1][['uJ']].mean(1)) , marker = "s",  vmin=min_j, vmax=max_j, s=sym_marker_size)
 
-#axes.plot(dataN['acc']['test3'], label = 'FP (94.10%)',linewidth=2)
-#axes.plot(dataB['acc']['test3'], label = 'Quantized (91.67%)',linewidth=2)
-axes.set_xlabel('uJ')
-axes.set_ylabel('Accuracy')
-axes.legend()
+axes.set_xlabel('')
+axes.set_ylabel('# Bits/Blocks/Hidden')
+
+plt.xticks([.5, 1., 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
+
+
+labels = [item.get_text() for item in axes.get_xticklabels()]
+labels[0] = 'Input Bits (M0)'
+labels[1] = 'Input Bits (M1)'
+labels[2] = 'Output Bits (M0)'
+labels[3] = 'Output Bits (M1)'
+labels[4] = 'Non CIM Bits (M0)'
+labels[5] = 'Non CIM Bits (M1)'
+labels[6] = 'Blocks (M0)'
+labels[7] = 'Blocks (M1)'
+labels[8] = 'Hidden (M0)'
+labels[9] = 'Hidden (M1)'
+
+axes.set_xticklabels(labels)
+
+plt.xticks(rotation=55,ha='right')
+cbar = plt.colorbar(sc1)
+cbar.ax.set_xlabel('uJ')
 
 plt.legend(frameon=False)
 plt.tight_layout()
-plt.savefig('big_scatter.png')
+plt.savefig('j_abl.png')
 plt.close()
-
--- sweeps.py
+++ sweeps.py
@@ -159,7 +159,7 @@ part4 = ".txt\nmodule load python\nsetenv OMP_NUM_THREADS $NSLOTS\npython KWS_LS
 ################
 
 #'quant-actNM':[2,3,4]} #, 
-sweep_parameters = {'n-msb':[1,2,3,4,5,6,7,8,9,10,11,12], 'hidden':[114,200,300,400,500]} #,5,6,7,8,9,10,11,12
+sweep_parameters = {'hidden':[114,200,300,400,500]} #,5,6,7,8,9,10,11,12
 
 
 #sweep_parameters = {'win-length':[640, 641, 654, 668, 682, 696, 712, 728, 746, 762, 782, 801, 822, 844, 866, 890, 916]}
@@ -173,19 +173,19 @@ avail_q = ['gpu@@joshi']
 q_counter = 0
 
 
-# for i in range(trials):
-#     for variable in sweep_parameters:
-#         for value in sweep_parameters[variable]:
-#             name = ident_word + "_M0" +variable + "_" + str(value).replace(",","")   + "_" + str(i)
-#             with open('jobscripts/'+name+'.script', 'w') as f:
-#                 if isinstance(value, str):
-#                     f.write(part1 + avail_q[q_counter] + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " \"" + str(value)+ "\" --random-seed " + str(random_seeds[i]) + " --max-w 1 --method 0 --quant-actNM 6 --quant-actMVM 6 --quant-inp 6 --quant-w 6"  ) 
-#                 else:
-#                     f.write(part1 + avail_q[q_counter] + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " \"" + str(value)+ "\" --random-seed " + str(random_seeds[i]) + " --max-w 1 --method 0 --quant-actNM 6 --quant-actMVM 6 --quant-inp 6 --quant-w 6"  ) 
-#             os.system("qsub "+ 'jobscripts/'+name+'.script')
-#             q_counter += 1
-#             if q_counter >= len(avail_q):
-#                 q_counter = 0
+for i in range(trials):
+    for variable in sweep_parameters:
+        for value in sweep_parameters[variable]:
+            name = ident_word + "_M0" +variable + "_" + str(value).replace(",","")   + "_" + str(i)
+            with open('jobscripts/'+name+'.script', 'w') as f:
+                if isinstance(value, str):
+                    f.write(part1 + avail_q[q_counter] + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " \"" + str(value)+ "\" --random-seed " + str(random_seeds[i]) + " --max-w 1 --batch-size 100 --method 0 --quant-actNM 6 --quant-actMVM 6 --quant-inp 6 --quant-w 6"  ) 
+                else:
+                    f.write(part1 + avail_q[q_counter] + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " \"" + str(value)+ "\" --random-seed " + str(random_seeds[i]) + " --max-w 1 --method 0 --batch-size 100 --quant-actNM 6 --quant-actMVM 6 --quant-inp 6 --quant-w 6"  ) 
+            os.system("qsub "+ 'jobscripts/'+name+'.script')
+            q_counter += 1
+            if q_counter >= len(avail_q):
+                q_counter = 0
 
 
 
@@ -195,9 +195,9 @@ for i in range(trials):
             name = ident_word + "_M1" +variable + "_" + str(value).replace(",","")   + "_" + str(i)
             with open('jobscripts/'+name+'.script', 'w') as f:
                 if isinstance(value, str):
-                    f.write(part1 + avail_q[q_counter] + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " \"" + str(value)+ "\" --random-seed " + str(random_seeds[i]) + " --max-w 1 --method 1 --quant-actNM 6 --quant-actMVM 6 --quant-inp 6 --quant-w 6"  ) 
+                    f.write(part1 + avail_q[q_counter] + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " \"" + str(value)+ "\" --random-seed " + str(random_seeds[i]) + " --max-w 1 --method 1 --batch-size 100 --quant-actNM 6 --quant-actMVM 6 --quant-inp 6 --quant-w 6"  ) 
                 else:
-                    f.write(part1 + avail_q[q_counter] + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " \"" + str(value)+ "\" --random-seed " + str(random_seeds[i]) + " --max-w 1 --method 1 --quant-actNM 6 --quant-actMVM 6 --quant-inp 6 --quant-w 6"  ) 
+                    f.write(part1 + avail_q[q_counter] + part11  + name + part2 + name + part3 + name + part4 + " --" + variable + " \"" + str(value)+ "\" --random-seed " + str(random_seeds[i]) + " --max-w 1 --method 1 --batch-size 100 --quant-actNM 6 --quant-actMVM 6 --quant-inp 6 --quant-w 6"  ) 
             os.system("qsub "+ 'jobscripts/'+name+'.script')
             q_counter += 1
             if q_counter >= len(avail_q):
