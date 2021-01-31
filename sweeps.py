06d2cd1c99fada0393ce482ea2adc903c52cfa75
Clemens Schaefer <15638937+clee1994@users.noreply.github.com>
2021-01-30 22:59:11 -0500

    results

--- sweeps.py
+++ sweeps.py
@@ -116,7 +116,7 @@ part4 = ".txt\nmodule load python\nsetenv OMP_NUM_THREADS $NSLOTS\npython KWS_LS
 # #sweep_parameters = {'win-length':[640, 641, 654, 668, 682, 696, 712, 728, 746, 762, 782, 801, 822, 844, 866, 890, 916]}
 
 
-sweep_parameters = {'quant-w':[4,6,8]}
+sweep_parameters = {'quant-w':[3]}
 # sweep_parameters = {'quant-inp':[2,3,4,5,6,7,8], 'quant-actMVM':[2,3,4,5,6,7,8], 'quant-actNM':[2,3,4,5,6,7,8,9,10,11,12], 'n-msb':[1,2,3,4,5,6]}
 trials = 3
 
